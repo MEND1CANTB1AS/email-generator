0f67cd7b50af7113e1784525b74d5b935d655409
MEND1CANTB1AS <74216076+MEND1CANTB1AS@users.noreply.github.com>
2021-02-02 22:18:21 -0700

    email

--- emailapp/Email.java
+++ emailapp/Email.java
@@ -7,21 +7,49 @@ public class Email {
   private String lastName;
   private String password;
   private String department;
-  private int mailbocCapacity;
+  private String emailNumber;
+  private String email;
+  private int mailboxCapacity;
+  private int defaultPasswordLength = 10;
+  private int emailNumeberLength = 4;
+  private String schoolSuffix = "byui.edu";
   //private String alternateEmail;
 
   //Constructor to receive the first naem and last name
   public Email(String firstName, String lastName) {
-    this.firstName = firstName;
+    char firstNameLetter = firstName.charAt(0);
+    firstNameLetter = Character.toLowerCase(firstNameLetter);
     this.lastName = lastName;
-    System.out.println("Email Created: " + this.firstName + " " + this.lastName); 
+    //System.out.println("Email Created: " + firstNameLetter + this.lastName); 
   
-    //Call a method asking for the departmnet - return the department
-    this.department = setDepartment();
-    System.out.println("Department: " + this.department);
+    //Call a method to generate a random 4 digit number
+    // this.department = setDepartment();
+    // System.out.println("Department: " + this.department);
+
+    this.emailNumber = setEmailNumber(emailNumeberLength);
+    //System.out.println("Number: " + this.emailNumber);
 
     //Call a method that ruturns a random password
+    this.password = randomPassword(defaultPasswordLength);
+    System.out.println("Your password is: " + this.password);
+
+    //Combine elements to generate email
+    email = firstNameLetter + lastName.toLowerCase() + "." + emailNumber + "@" + schoolSuffix;
+    System.out.println("Your email is: " + email);
   }
+  //Generate random 4 digit number
+  private String setEmailNumber(int length) {
+    String numberSet = "1234567890";
+    char[] emailNumber = new char[length];
+    for (int i=0; i<length; i++) {
+      int rand = (int) (Math.random() * numberSet.length());
+      emailNumber[i] = numberSet.charAt(rand);
+    }
+    return new String(emailNumber);
+  }
+      //System.out.println(rand);
+  
+  
   //Ask for the department
   private String setDepartment() {
     System.out.print("Enter the department\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\nEnter department code: ");
@@ -39,8 +67,10 @@ public class Email {
     for (int i=0; i<length; i++) {
       int rand = (int) (Math.random() * passwordSet.length());
       password[i] = passwordSet.charAt(rand);
+      //System.out.println(rand);
+      //System.out.println(passwordSet.charAt(rand));
     }
-    
+    return new String(password);
   }
   //Set mailbox capacity
 
--- emailapp/EmailApp.java
+++ emailapp/EmailApp.java
@@ -1,8 +1,23 @@
 package emailapp;
 
+import java.util.Scanner;
+
 public class EmailApp {
-    public static void main(String[] args) {
-        Email em1 = new Email("John","Smith");
-            
+    
+    public static void main(String[] args){
+        
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter the student's last name: ");
+        String lastName = scan.nextLine();
+
+        System.out.println("Please enter the student's first name: ");
+        String firstName = scan.nextLine();
+        System.out.println();
+        // public static void main(String[] args) {
+        Email em1 = new Email(firstName,lastName);
+                
+        scan.close();
     }
+    
 }
+
