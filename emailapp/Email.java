aa93070d63e317a10f4f9a490a07aace514cbea2
MEND1CANTB1AS <74216076+MEND1CANTB1AS@users.noreply.github.com>
2021-02-02 22:44:17 -0700

    lowercase letters to password

--- emailapp/Email.java
+++ emailapp/Email.java
@@ -6,7 +6,6 @@ public class Email {
   private String firstName;
   private String lastName;
   private String password;
-  private String department;
   private String emailNumber;
   private String email;
   private int mailboxCapacity;
@@ -23,9 +22,6 @@ public class Email {
     //System.out.println("Email Created: " + firstNameLetter + this.lastName); 
   
     //Call a method to generate a random 4 digit number
-    // this.department = setDepartment();
-    // System.out.println("Department: " + this.department);
-
     this.emailNumber = setEmailNumber(emailNumeberLength);
     //System.out.println("Number: " + this.emailNumber);
 
@@ -50,19 +46,10 @@ public class Email {
       //System.out.println(rand);
   
   
-  //Ask for the department
-  private String setDepartment() {
-    System.out.print("Enter the department\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\nEnter department code: ");
-    Scanner in = new Scanner(System.in);
-    int depChoice =in.nextInt();
-    if (depChoice == 1) {return "sales";}
-    else if (depChoice ==2) {return "dev";}
-    else if (depChoice == 3) {return "acct";}
-    else {return "";}
-  }
+
   //Generate a random password
   private String randomPassword(int length) {
-    String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*";
+    String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*";
     char[] password = new char[length];
     for (int i=0; i<length; i++) {
       int rand = (int) (Math.random() * passwordSet.length());
--- test.java
+++ test.java
@@ -1,11 +1,25 @@
-import java.util.Scanner;
+// import java.util.Scanner;
 
-class Main {
-    public static void main(String[] args) {
-      Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-      System.out.println("Enter username");
+// private String department;
+//     // this.department = setDepartment();
+//     // System.out.println("Department: " + this.department);
+//   //Ask for the department
+//   private String setDepartment() {
+//     System.out.print("Enter the department\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\nEnter department code: ");
+//     Scanner in = new Scanner(System.in);
+//     int depChoice =in.nextInt();
+//     if (depChoice == 1) {return "sales";}
+//     else if (depChoice ==2) {return "dev";}
+//     else if (depChoice == 3) {return "acct";}
+//     else {return "";}
+//   }
+
+// class Main {
+//     public static void main(String[] args) {
+//       Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+//       System.out.println("Enter username");
   
-      String userName = myObj.nextLine();  // Read user input
-      System.out.println("Username is: " + userName);  // Output user input
-    }
-  }
+//       String userName = myObj.nextLine();  // Read user input
+//       System.out.println("Username is: " + userName);  // Output user input
+//     }
+//   }
